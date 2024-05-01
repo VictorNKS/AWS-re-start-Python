518b35da023e082d6a358b06e62eea9d7448650d
VictorNKS <vpsnks2@gmail.com>
2024-05-01 13:12:20 -0300

    Update AWS lab 01.py
    Correções

--- AWS lab 01.py
+++ AWS lab 01.py
@@ -1,5 +1,3 @@
 print("Hello,AWS re/start")
 
-"""
-this repository i will commit and push every Lab on my learning path on AWS re/start Python
-"""
+# É usado o hash em Python para inserir dados que não vao ser lidos pelo codigo
