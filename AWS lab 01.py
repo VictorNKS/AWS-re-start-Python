dc8e09eabae516df983124db7c7b19f448fda393
VictorNKS <vpsnks2@gmail.com>
2024-05-01 13:52:54 -0300

    Update AWS lab 01.py
    Learning branch and correction of files

--- AWS lab 01.py
+++ AWS lab 01.py
@@ -1,5 +1,3 @@
 print("Hello,AWS re/start")
 
-"""
-this repository i will commit and push every Lab on my learning path on AWS re/start Python
-"""
+#Right here i did a correction on the file and commit/push inside the main branch
