a87e7f84bc5dd76d3da6e33d95c3141a78d78098
VictorNKS <vpsnks2@gmail.com>
2024-05-01 14:08:20 -0300

    Final Update
    Terminei de fazer os testes do Github e encerro o lab 1 que seria apenas para usar print de uma frase

--- AWS lab 01.py
+++ AWS lab 01.py
@@ -1,3 +1,7 @@
 print("Hello,AWS re/start")
 
 #Right here i did a correction on the file and commit/push inside the main branch
+
+#A partir de agora, feito a simulação de versionamento do github onde pessoa A altera o codigo da Pessoa B no branch "Usando-GitHub-para-criar-uma-Branch" e fazendo o push no main branch, termino meus testes feitos em paralelo com o AWS re/start lab 1
+
+print("Hello PHYTHON!")
