98c077cb7b2e2e313d20006b753eb35663c2bdc3
VictorNKS <vpsnks2@gmail.com>
2024-05-12 14:05:57 -0300

    13 (Encrypted/Decypted )

---
+++ AWS lab 13.py
@@ -0,0 +1,50 @@
+#def... define uma função que no casa é atribuilda a variavel getDoubleAlphabet(alphabet)... a variavel depois do def pode ter qualquer valor
+def getDoubleAlphabet(alphabet):
+    doubleAlphabet = alphabet + alphabet
+    return doubleAlphabet
+#atribuindo um valor para alphabet (pode ser qualquer valor)
+#atribuindo uma variavel para o getDoubleAlphabet (pode ser qualquer nome)
+#Valornumerico = getDoubleAlphabet(alphabet)
+
+#encriptando uma mensagem
+def getMessage():
+    stringToEncrypt = input("Please enter a message to encrypt: ")
+    return stringToEncrypt
+
+#Fazendo uma cipher key
+def getCipherKey():
+    shiftAmount = input( "Please enter a key (whole number from 1-25): ")
+    return shiftAmount
+
+def encryptMessage(message, cipherKey, alphabet):
+    encryptedMessage = ""
+    uppercaseMessage = ""
+    uppercaseMessage = message.upper()
+    for currentCharacter in uppercaseMessage:
+        position = alphabet.find(currentCharacter)
+        newPosition = position + int(cipherKey)
+        if currentCharacter in alphabet:
+            encryptedMessage = encryptedMessage + alphabet[newPosition]
+        else:
+            encryptedMessage = encryptedMessage + currentCharacter
+    return encryptedMessage
+
+def decryptMessage(message, cipherKey, alphabet):
+    decryptKey = -1 * int(cipherKey)
+    return encryptMessage(message, decryptKey, alphabet)
+
+def runCaesarCipherProgram():
+    myAlphabet="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    print(f'Alphabet: {myAlphabet}')
+    myAlphabet2 = getDoubleAlphabet(myAlphabet)
+    print(f'Alphabet2: {myAlphabet2}')
+    myMessage = getMessage()
+    print(myMessage)
+    myCipherKey = getCipherKey()
+    print(myCipherKey)
+    myEncryptedMessage = encryptMessage(myMessage, myCipherKey, myAlphabet2)
+    print(f'Encrypted Message: {myEncryptedMessage}')
+    myDecryptedMessage = decryptMessage(myEncryptedMessage, myCipherKey, myAlphabet2)
+    print(f'Decypted Message: {myDecryptedMessage}')
+
+runCaesarCipherProgram()
